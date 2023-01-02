9b13b1be15ed441d4dc100dcf856605ee48aa3e3
soves <nidi.same@gmail.com>
2023-01-02 20:21:33 +0200

    rustfmt width

--- src/forsen_lines.rs
+++ src/forsen_lines.rs
@@ -1,11 +1,16 @@
 use csv::Reader;
-use rand::{rngs::ThreadRng, seq::SliceRandom, thread_rng};
+use rand::{
+    rngs::ThreadRng, seq::SliceRandom, thread_rng,
+};
 use std::{
     error::Error,
     path::{Path, PathBuf},
 };
 
-fn parse_lines(path: PathBuf, pepepains: &mut ForsenLines) -> Result<(), Box<dyn Error>> {
+fn parse_lines(
+    path: PathBuf,
+    pepepains: &mut ForsenLines,
+) -> Result<(), Box<dyn Error>> {
     let mut rdr = Reader::from_path(path)?;
     for result in rdr.records() {
         let record = result?;
@@ -33,15 +38,19 @@ pub struct ForsenLines {
 
 impl ForsenLines {
     pub fn new(path: PathBuf) -> Self {
-        let mut result = Self { lines: Vec::new() };
-        parse_lines(path, &mut result).expect("Error parsing csv");
+        let mut result =
+            Self { lines: Vec::new() };
+        parse_lines(path, &mut result)
+            .expect("Error parsing csv");
         result
     }
 
     pub fn get_random(&self) -> String {
         let mut rng = thread_rng();
         self.lines
-            .choose_weighted(&mut rng, |item| item.1)
+            .choose_weighted(&mut rng, |item| {
+                item.1
+            })
             .unwrap()
             .0
             .clone()
