26688043420f1b285f79dc79a9f984b794bcc6b5
soves <nidi.same@gmail.com>
2023-04-03 21:19:44 +0300

    project updated to new shuttle version

--- src/db.rs
+++
@@ -1,7 +0,0 @@
-use sqlx::{FromRow, PgPool};
-use std::fmt::Write;
-
-#[derive(FromRow)]
-struct Guild {
-    id: u64,
-}
--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,13 @@ use forsen_lines::ForsenLines;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::model::prelude::command::Command;
-use serenity::model::prelude::interaction::{
-    Interaction, InteractionResponseType,
-};
+use serenity::model::prelude::interaction::{Interaction, InteractionResponseType};
 use serenity::prelude::*;
-use serenity::{
-    async_trait, model::prelude::GuildId,
-};
+use serenity::{async_trait, model::prelude::GuildId};
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
 
 mod commands;
-mod db;
 mod forsen_lines;
 
 use commands::channel_id::*;
@@ -30,36 +25,20 @@ struct Bot;
 
 #[async_trait]
 impl EventHandler for Bot {
-    async fn message(
-        &self,
-        ctx: Context,
-        msg: Message,
-    ) {
+    async fn message(&self, ctx: Context, msg: Message) {
         if msg.author.bot {
             return;
         }
 
-        if msg
-            .content
-            .to_lowercase()
-            .contains("forsen")
-        {
-            if let Err(e) = msg
-                .channel_id
-                .say(&ctx.http, "forsen")
-                .await
-            {
-                error!(
-                    "Error sending message: {:?}",
-                    e
-                );
+        if msg.content.to_lowercase().contains("forsen") {
+            if let Err(e) = msg.channel_id.say(&ctx.http, "forsen").await {
+                error!("Error sending message: {:?}", e);
             }
         }
 
         if msg.content.contains(":Painsge:") {
             let line = {
-                let data_read =
-                    ctx.data.read().await;
+                let data_read = ctx.data.read().await;
                 data_read
                     .get::<PepePains>()
                     .expect("Expected PepePains")
@@ -67,42 +46,19 @@ impl EventHandler for Bot {
             }
             .get_random();
 
-            if let Err(e) = msg
-                .channel_id
-                .say(&ctx.http, line)
-                .await
-            {
-                error!(
-                    "Error sending message: {:?}",
-                    e
-                );
+            if let Err(e) = msg.channel_id.say(&ctx.http, line).await {
+                error!("Error sending message: {:?}", e);
             }
         }
     }
 
-    async fn interaction_create(
-        &self,
-        ctx: Context,
-        interaction: Interaction,
-    ) {
-        if let Interaction::ApplicationCommand(
-            command,
-        ) = interaction
-        {
-            let content = match command
-                .data
-                .name
-                .as_str()
-            {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
                 "clear-commands" => {
-                    commands::clear_commands::run(
-                        &command.data.options,
-                        &ctx,
-                    )
-                    .await
+                    commands::clear_commands::run(&command.data.options, &ctx).await
                 }
-                _ => "not implemented :("
-                    .to_string(),
+                _ => "not implemented :(".to_string(),
             };
 
             if let Err(why) = command
@@ -118,15 +74,8 @@ impl EventHandler for Bot {
         }
     }
 
-    async fn ready(
-        &self,
-        ctx: Context,
-        ready: Ready,
-    ) {
-        info!(
-            "{} is connected!",
-            ready.user.name
-        );
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
 
         let guild_command = Command::create_global_application_command(&ctx.http, |command| {
             commands::channel_id::register(command)
@@ -135,30 +84,20 @@ impl EventHandler for Bot {
     }
 }
 
-#[shuttle_service::main]
+#[shuttle_runtime::main]
 async fn serenity(
-    #[shuttle_static_folder::StaticFolder(
-        folder = "static"
-    )]
-    static_folder: PathBuf,
-    #[shuttle_secrets::Secrets]
-    secret_store: SecretStore,
-) -> shuttle_service::ShuttleSerenity {
+    #[shuttle_static_folder::StaticFolder(folder = "static")] static_folder: PathBuf,
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
     // Get the discord token set in `Secrets.toml`
-    let token = if let Some(token) =
-        secret_store.get("DISCORD_TOKEN")
-    {
+    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
         token
     } else {
-        return Err(anyhow!(
-            "'DISCORD_TOKEN' was not found"
-        )
-        .into());
+        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
     };
 
     // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let client = Client::builder(&token, intents)
         .event_handler(Bot)
@@ -167,13 +106,10 @@ async fn serenity(
 
     {
         let mut data = client.data.write().await;
-        data.insert::<PepePains>(Arc::new(
-            ForsenLines::new(
-                static_folder
-                    .join("forsen_lines.csv"),
-            ),
-        ));
+        data.insert::<PepePains>(Arc::new(ForsenLines::new(
+            static_folder.join("forsen_lines.csv"),
+        )));
     }
 
-    Ok(client)
+    Ok(client.into())
 }
